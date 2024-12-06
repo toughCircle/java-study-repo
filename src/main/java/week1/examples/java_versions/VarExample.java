c4d587d2c3622a13d295fc7edffea8295ac0fa95
toughCircle <toughCircle@gmail.com>
2024-12-06 15:03:57 +0900

    feat: Java 11 - Var 예제 코드 (#1)

---
+++ src/main/java/week1/examples/java_versions/VarExample.java
@@ -0,0 +1,8 @@
+package week1.examples.java_versions;
+
+public class VarExample {
+	public static void main(String[] args) {
+		var message = "Hello, Java 11";
+		System.out.println(message);
+	}
+}
